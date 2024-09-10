0568ded241b8123915a0db34a2f3639338fdcf77
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-09-10 23:10:17 +0500

    taylor series

--- recursion/taylor.cpp
+++ recursion/taylor.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-using namespace  std;
+using namespace std;
 
-
-//calculation of taylor series--> we will use static valriables to solve taylor series
+// calculation of taylor series--> we will use static valriables to solve taylor series --> O(n^2)
 
 // double ty(double x, double n){
 //     static double P=1, F=1;
@@ -17,14 +16,40 @@ using namespace  std;
 //     }
 // }
 
+// Taylor series by using Horner's Rule-->O(n)--> can be done in ascending order
 
+// double ty(double x, double n ){
+//     static double S;
+// if (n==0)
+// {
+//     return S;
+// }
+// S=1+x/n*S;
+// return ty(x,n-1);
+// }
 
+// Horner's rule by using loop
+
+double ty(double x, double n)
+{
+    double S = 1;
+    int i;
+    double num = 1;
+    double den = 1;
+    for (i = 1; i <= n; i++)
+    {
+        num *= x;
+        den *= i;
+        S += num / den;
+
+    }
+    return S;
+}
 
 int main()
 {
-   
-cout<<ty(1,10);
 
+    cout << ty(1, 10);
 
     return 0;
 }
