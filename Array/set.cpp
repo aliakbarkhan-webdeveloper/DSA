63fc9b7bcd73492c28a54ae659e047222b79f9ca
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-09-28 22:57:22 +0500

    set

---
+++ Array/set.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int arr[5]={1,2,3,4,5};
+
+void set(int idx,int val){
+if (idx>=0)
+{
+    arr[idx]=val;
+    return;
+}
+
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
