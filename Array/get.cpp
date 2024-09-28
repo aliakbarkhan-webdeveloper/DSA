a93b072fba2f6ad7f7e7b64397a1f9755d3deecd
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-09-28 22:05:54 +0500

    get method

---
+++ Array/get.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int arr[10]={34,67,23,21,0,7,3,0};
+
+int get(int index){
+return arr[index];
+
+}
+
+int main(int argc, char const *argv[])
+{
+    
+cout<<get(5);
+
+    return 0;
+}
