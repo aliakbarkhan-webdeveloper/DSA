bc5398cf74baec8df8bcdccc0c9fbeb3155cf103
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-09-28 23:16:34 +0500

    sum

---
+++ Array/sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int sum(int arr[], int n)
+{
+
+    if (n < 0)
+    {
+        return 0;
+    }
+    static int total = 0;
+
+    int t = sum(arr, n - 1);
+    total = arr[n] + t;
+    return total;
+}
+
+int main()
+{
+    int arr[] = {5, 5, 5, 5, 5, 5};
+    cout << sum(arr, 5);
+    return 0;
+}
