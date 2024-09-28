e595bb810b6793136c7b238a376a3665de12a9f2
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-09-28 22:06:11 +0500

    get

--- Array/get.cpp
+++ Array/get.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int arr[10] = {34, 67, 23, 21, 0, 7, 3, 0};
 
-int arr[10]={34,67,23,21,0,7,3,0};
-
-int get(int index){
-return arr[index];
-
+int get(int index)
+{
+    return arr[index];
 }
 
 int main(int argc, char const *argv[])
 {
-    
-cout<<get(5);
+
+    cout << get(5);
 
     return 0;
 }
