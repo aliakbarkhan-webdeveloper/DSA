b3d5ec28c708a26663accd59aa01983d596c20a5
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-09-28 22:59:00 +0500

    set

--- Array/set.cpp
+++ Array/set.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int arr[5]={1,2,3,4,5};
+int arr[5] = {1, 2, 3, 4, 5};
 
-void set(int idx,int val){
-if (idx>=0)
+void set(int idx, int val)
 {
-    arr[idx]=val;
-    return;
-}
-
+    if (idx >= 0)
+    {
+        arr[idx] = val;
+        return;
+    }
 }
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+
+    cout << arr[4] << "\n";
+    set(4, 666666);
+    cout << arr[4] << "\n";
     return 0;
 }
