54f6a2180c2dade3ae04ba9672dbde03cb23f395
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-09-30 21:55:31 +0500

    Union

---
+++ Array/union.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// when arrays are not sorted  --> O(n^2)
+int arrA[5]={};
+int arrB[5]={};
+int unsortedUnion[10]={};
+void unsortUnion() {
+
+};
+
+// Whwn arrays are sorted  -->O(n)
+int arrC[5]={};
+int arrD[5]={};
+int sortedUnion[10]={};
+
+void sortedUnion() {
+
+};
+
+int main()
+{
+
+    return 0;
+}
