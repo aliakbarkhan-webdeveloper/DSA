244776a5d5c619068567976d84bd21000bb0418a
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-10-09 22:03:08 +0500

    Duplicate

--- Array/duplicates.cpp
+++ Array/duplicates.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int arr[6] = {1, 3, 3, 5, 6, 6};
+int n = 6;
 
-
-
-
-
-
+void duplicate()
+{
+    int duplicate = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == arr[i + 1] && arr[i] != duplicate)
+        {
+            cout << "The duplicate is: " << arr[i] << "\n";
+            duplicate = arr[i];
+        }
+    }
+}
 
 int main()
 {
-    
-
-
-
-
-
-
+    duplicate();
     return 0;
 }
